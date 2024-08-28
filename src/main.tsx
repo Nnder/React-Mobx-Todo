d88c4dcea42560743c69fa5224e3bd8e7d056e31
nnder <asfahaikrkt6k56@yandex.ru>
2024-08-28 10:42:02 +0500

    feat(app): mobx example

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
+import { makeAutoObservable } from 'mobx'
+
+class Timer {
+  secondsPassed = 0
+
+  constructor() {
+      makeAutoObservable(this)
+  }
+
+  increaseTimer() {
+      this.secondsPassed += 1
+  }
+}
+
+export const myTimer = new Timer()
+
+setInterval(()=>{
+  myTimer.increaseTimer()
+}, 1000)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>,
 )
