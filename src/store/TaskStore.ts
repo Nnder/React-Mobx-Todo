74239f19f2f0355f98623f1f315b1c2721ca796b
nnder <asfahaikrkt6k56@yandex.ru>
2024-09-03 13:31:33 +0500

    feat(app): delete task and update id

--- src/store/TaskStore.ts
+++ src/store/TaskStore.ts
@@ -3,10 +3,10 @@ import { Task } from '../types/types'
 
 class TaskStore {
     tasks: Task[] = [
-        {id: '1', body: '123', title: '123232323232323', completed: false, children: [], parent: null},
-        {id: '2', body: '122333', title: '12323232323', completed: false, children: [], parent: null},
-        {id: '3', body: '32', title: '1', completed: false, children: [], parent: null},
-        {id: '4', body: '32323', title: '123', completed: false,  children: [], parent: null},
+        {id: '1', title: 'Задача 1', body: 'Тело задача 1', completed: false, children: [], parent: null},
+        {id: '2', title: 'Задача 2', body: 'Тело задача 2', completed: false, children: [], parent: null},
+        {id: '3', title: 'Задача 3', body: 'Тело задача 3', completed: false, children: [], parent: null},
+        {id: '4', title: 'Задача 4', body: 'Тело задача 4', completed: false, children: [], parent: null},
     ] 
 
     selectedTask: Task | null = null;
@@ -24,18 +24,22 @@ class TaskStore {
         this.completeParent(parent)
     }
 
-    deleteTask(task: Task, tasks : Task[] = this.tasks){
-        const newTasks = tasks.filter(t => {
-            if(task.id === t.id){
-                return false;
-            } else if(t.children && t.children.length > 0){
-                t.children = this.deleteTask(task, t.children);
-                return true;
-            }
-            return true;
-        })
+    deleteTask(task: Task){
+        if(task.parent){
+            const filteredTasks = task.parent.children
+            .filter(child => child.id !== task.id)
 
-        return newTasks
+            // после удаления обновить id задач
+            filteredTasks.map((child, index) => {
+                const ids =  child.id.split('.')
+                ids[ids.length - 1] = index.toString()
+                child.id = ids.join('.');
+            })
+
+            task.parent.children = filteredTasks;
+        } else {
+            this.tasks = this.tasks.filter((child)=> child.id !== task.id)
+        }
     }
 
     updateTask(task: Task, title: string, body: string) {
@@ -44,22 +48,22 @@ class TaskStore {
     }
 
     completeParent(task: Task){
+        // если каждая дочерняя задача выполнена то родительская задача тоже выполняется
         task.completed = task.children.every(child => child.completed === true);
-
         if (task.parent) {
             this.completeParent(task.parent);
-            // task.parent.completed = task.children.every(child => child.completed === true);
         }
     }
 
     completeTask(task: Task, completed: boolean) {
+        // обновляем статус и меняем статус у дочерних задач
         task.completed = completed;
         if (task.children && task.children.length > 0) {
             for (let subtask of task.children) {
                 this.completeTask(subtask, completed);
             }
         }
-
+        // проверяем может ли родитель изменить свой статус
         if (task.parent) {
             this.completeParent(task.parent);
         }
@@ -68,7 +72,6 @@ class TaskStore {
     setSelectedTask(task:Task) {
         this.selectedTask = task;
     }
-
 }
 
 export default new TaskStore();
